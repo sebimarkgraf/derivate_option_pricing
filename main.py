da2de7bfc1fc67289eeadb4b99424b76cc6feb7a
Sebastian Markgraf <Sebastian-Markgraf@t-online.de>
2022-07-10 14:12:42 +0200

    Add comments to make code easier to understand

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
-from option_pricing import Underlying, BinomialModel, BlackScholesModel, Option
 import matplotlib.pyplot as plt
-from tqdm import tqdm
 import numpy as np
+from tqdm import tqdm
+
+from option_pricing import BinomialModel, BlackScholesModel, Option, Underlying
+
 plt.style.use("bmh")
 
-underlying = Underlying(base_beginning=13144.28, volatility=0.2414, dividend=0.00, interest_rate=-0.00517)
+underlying = Underlying(
+    base_beginning=13144.28, volatility=0.2414, dividend=0.00, interest_rate=-0.00517
+)
 option = Option(underlying, strike_price=14000.0, is_call=False, american=False, T=0.5)
 
-
+# Use the binomial model to calculate the option price
 price, option_tree, stock_tree = BinomialModel(periods=100).calc_option_price(option)
 print(f"b) Binomial Price bei n=100: {price / 100}")
 
-## Aufgabe c)
+# Aufgabe c)
 prices = []
 ns = []
-for n in tqdm(np.logspace(0, 3, num=100, dtype='int')):
+# We walk through the logspace of periods to use less time.
+# This gives equidistant points for the plotting later on
+for n in tqdm(np.logspace(0, 3, num=100, dtype="int")):
     price, option_tree, stock_tree = BinomialModel(periods=n).calc_option_price(option)
     prices.append(price / 100)
     ns.append(n)
 
 fix, ax = plt.subplots()
+# Plot the binomial price curve against the periods
 ax.plot(ns, prices, label="Binomial")
 
-
+# Use the black scholes model to calculate the current option price
 black_scholes = BlackScholesModel().calc_option_price(option=option)
 print(f"Black Scholes Price: {black_scholes / 100}")
 
-plt.axhline(black_scholes / 100, color='r', label="Black Scholes")
-plt.axhline(13.08, color='g', label="Markt")
+# Add the black scholes price as a red line into the plot
+plt.axhline(black_scholes / 100, color="r", label="Black Scholes")
 
-ax.set(xlabel="Binomial Perioden", ylabel="Option Preis (€)", title="Preis von DV7PCH", xscale="log")
+# Add the market price
+plt.axhline(13.08, color="g", label="Markt")
+
+# Plot styling
+ax.set(
+    xlabel="Binomial Perioden",
+    ylabel="Option Preis (€)",
+    title="Preis von DV7PCH",
+    xscale="log",
+)
 ax.legend()
-plt.savefig('price.png')
+plt.savefig("price.png")
 
 
 # f)
-
